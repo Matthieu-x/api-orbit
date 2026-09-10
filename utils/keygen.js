7c755b67efb0d775fbd60cf35d6f96750c81aba9
Matthieu <cololacalempira5@gmail.com>
2026-09-09 19:39:16 -0600

    Actualizar keygen.js

--- utils/keygen.js
+++ utils/keygen.js
@@ -11,9 +11,14 @@ function generateApiKey() {
   return `ORBIT-${key}`;
 }
 
+function generateVerificationCode() {
+  const code = crypto.randomInt(0, 1000000);
+  return `ORBIT-${String(code).padStart(6, "0")}`;
+}
+
 function todayStamp() {
   const now = new Date();
   return `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, "0")}-${String(now.getDate()).padStart(2, "0")}`;
 }
 
-module.exports = { generateApiKey, todayStamp };
+module.exports = { generateApiKey, generateVerificationCode, todayStamp };
